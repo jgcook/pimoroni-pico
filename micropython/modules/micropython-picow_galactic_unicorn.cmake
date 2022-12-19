833a13135e726965f9ddc6d7db36efe983997b23
jgcook <34443196+jgcook@users.noreply.github.com>
2022-12-19 11:43:10 +0000

    Update micropython-picow_galactic_unicorn.cmake

--- micropython/modules/micropython-picow_galactic_unicorn.cmake
+++ micropython/modules/micropython-picow_galactic_unicorn.cmake
@@ -5,17 +5,17 @@ list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../")
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../")
 
 # Essential
-include(pimoroni_i2c/micropython)
-include(pimoroni_bus/micropython)
+#include(pimoroni_i2c/micropython)
+#include(pimoroni_bus/micropython)
 
 # Pico Graphics Essential
-include(hershey_fonts/micropython)
-include(bitmap_fonts/micropython)
-include(picographics/micropython)
+#include(hershey_fonts/micropython)
+#include(bitmap_fonts/micropython)
+#include(picographics/micropython)
 
 # Pico Graphics Extra
-include(jpegdec/micropython)
-include(qrcode/micropython/micropython)
+#include(jpegdec/micropython)
+#include(qrcode/micropython/micropython)
 
 # Sensors & Breakouts
 #include(breakout_dotmatrix/micropython)
@@ -42,12 +42,12 @@ include(qrcode/micropython/micropython)
 #include(pcf85063a/micropython)
 
 # Utility
-include(adcfft/micropython)
+#include(adcfft/micropython)
 
 # LEDs & Matrices
-include(galactic_unicorn/micropython)
+#include(galactic_unicorn/micropython)
 
-# include(micropython-common)
+include(micropython-common)
 
 function(enable_ulab)
     include(ulab/code/micropython)
@@ -98,4 +98,4 @@ NDARRAY_HAS_BINARY_OP_ADD=1
 endfunction()
 enable_ulab()
 
-include(modules_py/modules_py)
+#include(modules_py/modules_py)
