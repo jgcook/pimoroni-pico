7a72ca9e9bcc595b6ad42f5e639de2c378df9322
jgcook <34443196+jgcook@users.noreply.github.com>
2022-12-19 14:45:09 +0000

    Update micropython-picow_galactic_unicorn.cmake

--- micropython/modules/micropython-picow_galactic_unicorn.cmake
+++ micropython/modules/micropython-picow_galactic_unicorn.cmake
@@ -6,8 +6,7 @@ list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../")
 
 # Essential
 include(pimoroni_i2c/micropython)
-include(pimoroni_bus/micropython)
-
+#include(pimoroni_bus/micropython)
 # Pico Graphics Essential
 #include(hershey_fonts/micropython)
 #include(bitmap_fonts/micropython)
